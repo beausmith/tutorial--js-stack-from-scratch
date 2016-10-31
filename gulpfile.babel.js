0691d8b906ce519346b55aa905bf3afd35c0bbbc
Beau Smith <beau@beausmith.com>
2016-10-30 21:58:25 -0700

    7 - Client App with Webpack (Integrating Webpack to Gulp)

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,16 +4,26 @@
 import gulp from 'gulp'
 import babel from 'gulp-babel'
 import del from 'del'
-import { exec } from 'child_process'
 import eslint from 'gulp-eslint'
+import webpack from 'webpack-stream'
+import webpackConfig from './webpack.config.babel'
 
 const paths = {
-  allSrcJs: 'src/**/*.js',
+  allSrcJs: 'src/**/*.js?(x)',
+  serverSrcJs: 'src/server/**/*.js?(x)',
+  sharedSrcJs: 'src/shared/**/*.js?(x)',
+  clientEntryPoint: 'src/client/app.js',
+  clientBundle: 'dist/client-bundle.js?(.map)',
   gulpFile: 'gulpfile.babel.js',
+  webpackFile: 'webpack.config.babel.js',
   libDir: 'lib',
+  distDir: 'dist',
 }
 
-gulp.task('clean', () => del(paths.libDir))
+gulp.task('clean', () => del([
+  paths.libDir,
+  paths.clientBundle,
+]))
 
 gulp.task('build', ['lint', 'clean'], () =>
   gulp.src(paths.allSrcJs)
@@ -21,11 +31,10 @@ gulp.task('build', ['lint', 'clean'], () =>
     .pipe(gulp.dest(paths.libDir))
 )
 
-gulp.task('main', ['build'], callback =>
-  exec(`node ${paths.libDir}`, (error, stdout) => {
-    console.log(stdout)
-    return callback(error)
-  })
+gulp.task('main', ['lint', 'clean'], () =>
+  gulp.src(paths.clientEntryPoint)
+    .pipe(webpack(webpackConfig))
+    .pipe(gulp.dest(paths.distDir))
 )
 
 gulp.task('watch', () => {
@@ -38,6 +47,7 @@ gulp.task('lint', () =>
   gulp.src([
     paths.allSrcJs,
     paths.gulpFile,
+    paths.webpackFile,
   ])
     .pipe(eslint())
     .pipe(eslint.format())
